e6aca3c71882b56ad59152d41bb52525a3bbf7a0
mmulahvictor <mmulahvictor@gmail.com>
2022-06-13 09:39:48 +0300

    commit tree 3099185af91d83fa092760546e6486a3d774a60a

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const input = document.querySelector('input');
+const input = document.getElementById('input');
 
-function addingEventListener() {
-    input.addEventListener('click', function(event) {
-        return "clicked"
-      });
+const addingEventListener = () =>{
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
 }
